f18d3a12ddf91d907432240e4b1b9532f539b75e
Dzung Xuan Tran <dungxuan@quicklogix.com>
2017-11-04 18:20:28 +0700

    added multiple XML

--- src/com/quicklogix/helpers/BaseXConnector.java
+++ src/com/quicklogix/helpers/BaseXConnector.java
@@ -1,8 +1,10 @@
 package com.quicklogix.helpers;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -14,14 +16,21 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
+import com.ximpleware.ParseException;
+import com.ximpleware.VTDException;
+import com.ximpleware.VTDGen;
+import com.ximpleware.VTDNav;
+
 public class BaseXConnector {
     private final String FILENAME = "quicklogix";
     private static BaseXClient session;
+    private VTDGen vg;
 
     private BaseXConnector (){}
 
     public static BaseXConnector connect(String host,int port, String username, String password) {
         BaseXConnector instance = new BaseXConnector();
+        instance.vg = new VTDGen();
         try {
 			session = new BaseXClient(host, port, username, password);
 		} catch (IOException e) {
@@ -34,9 +43,16 @@ public class BaseXConnector {
         if (session == null) {
             throw new IOException("No connection");
         } else {
+        	try {
+                vg.setDoc(xml.getBytes(StandardCharsets.UTF_8));
+                vg.parse(false);
+                byte[] bytes = vg.getNav().getXML().getBytes();
                 String path = FILENAME + System.currentTimeMillis();
-                session.add(path, new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8.name())));
+                session.add(path, new ByteArrayInputStream(bytes));
                 return path;
+        	} catch (ParseException ex) {
+        		throw new IOException("Invalid XML");
+        	}
         }
     }
     
@@ -117,5 +133,59 @@ public class BaseXConnector {
             session.replace(pathName, new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8.name())));
             return true;
         }
+    }
+
+    public String addMultipleFile(String[] xmls) throws IOException, VTDException {
+		StringBuffer buffer = new StringBuffer();
+        Map<String, String> map = getContent(xmls[0]);
+        String footer = map.get("footer");
+        buffer.append(map.get("header"));
+        buffer.append(map.get("content"));
+        for (int i = 1; i < xmls.length; i++) {
+            map = getContent(xmls[i]);
+            if (footer.equalsIgnoreCase(map.get("footer"))) {
+                buffer.append(map.get("content"));
+            }
+        }
+        buffer.append(footer);
+        try {
+            vg.setDoc(buffer.toString().getBytes());
+            vg.parse(false);
+            byte[] bytes = vg.getNav().getXML().getBytes();
+            String path = FILENAME + System.currentTimeMillis();
+            session.add(path, new ByteArrayInputStream(bytes));
+            return path;
+        } catch (ParseException e) {
+//            e.printStackTrace();
+        	throw new IOException("Invalid XML");
+        }
+	}
+	
+	public Map<String, String> getContent(String xml) throws VTDException {
+        Map<String, String> map = new HashMap<>();
+        //parse XML
+        vg.setDoc(xml.getBytes());
+        vg.parse(false);
+        VTDNav vn = vg.getNav();
+        vn.toElement(VTDNav.ROOT);
+        long l = vn.getContentFragment();
+        byte[] bytes = vn.getXML().getBytes();
+
+        //get main content
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(bytes.length);
+        baos.write(bytes, (int) l, (int) (l>>32));
+        map.put("content", baos.toString());
+
+        //get header
+        baos.reset();
+        baos.write(bytes, 0, (int) l);
+        map.put("header", baos.toString());
+
+        //get footer
+        int lastIndex = xml.lastIndexOf("</");
+        map.put("footer", xml.substring(lastIndex));
+
+        vg.clear();
+        return map;
     }
 }
--- src/com/quicklogix/services/XMLService.java
+++ src/com/quicklogix/services/XMLService.java
@@ -8,39 +8,49 @@ import org.json.simple.JSONObject;
 
 import com.quicklogix.helpers.BaseXConnector;
 import com.quicklogix.propertiesfileloader.PropertyLoader;
+import com.ximpleware.VTDException;
 
 public class XMLService {
 	private BaseXConnector connection;
-    public XMLService() {
-    	PropertyLoader loader = new PropertyLoader();
-    	Properties props = loader.getpropObject();
-        connection = BaseXConnector.connect(
-        		props.getProperty("basexhost"), 
-        		Integer.parseInt(props.getProperty("basexport")),
-        		props.getProperty("basexusername"),
-        		props.getProperty("basexpassword")
-        		);
-    }
 
-    public String addXML(String dbName, String inputXML) throws IOException{
-    	connection.openDB(dbName);
-        return connection.addFile(inputXML);
-    }
-    
-    public String getXML(String dbName, String fileName) throws IOException {
-    	connection.openDB(dbName);
-    	Map<String, String> elements = connection.getFile(dbName, fileName);
-    	JSONObject jsonObj = new JSONObject(elements);
-    	return jsonObj.toString();
-    }
-    
-    public String[] getXpath(String dbName, String fileName) throws IOException {
-    	connection.openDB(dbName);
-    	return connection.getFileXPath(dbName, fileName);
-    }
-    
-    public boolean updateXML(String dbName, String pathName, String inputXML) throws IOException {
-    	connection.openDB(dbName);
-    	return connection.updateFile(dbName, pathName, inputXML);
-    }
+	public XMLService() {
+		PropertyLoader loader = new PropertyLoader();
+		Properties props = loader.getpropObject();
+		connection = BaseXConnector.connect(props.getProperty("basexhost"),
+				Integer.parseInt(props.getProperty("basexport")), props.getProperty("basexusername"),
+				props.getProperty("basexpassword"));
+	}
+
+	public String addXML(String dbName, String inputXML) throws IOException {
+		connection.openDB(dbName);
+		return connection.addFile(inputXML);
+	}
+
+	public String getXML(String dbName, String fileName) throws IOException {
+		connection.openDB(dbName);
+		Map<String, String> elements = connection.getFile(dbName, fileName);
+		JSONObject jsonObj = new JSONObject(elements);
+		return jsonObj.toString();
+	}
+
+	public String[] getXpath(String dbName, String fileName) throws IOException {
+		connection.openDB(dbName);
+		return connection.getFileXPath(dbName, fileName);
+	}
+
+	public boolean updateXML(String dbName, String pathName, String inputXML) throws IOException {
+		connection.openDB(dbName);
+		return connection.updateFile(dbName, pathName, inputXML);
+	}
+
+	public String addMultipleXML(String dbName, String[] inputXML)
+			throws IOException {
+		try {
+			connection.openDB(dbName);
+			return connection.addMultipleFile(inputXML);
+		} catch (VTDException ex) {
+			ex.printStackTrace();
+		}
+		return "";
+	}
 }
